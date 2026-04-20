7e9dcc9416ab8d26bb1e8e96970258d91de482f8
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-20 20:09:48 +0200

    Update sw.js
    koodia sw:hen

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
-// Service Worker
+const CACHE_NAME = 'pitkistool-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './kuva.png'
+];
+
+// Asennusvaihe: Tallennetaan perustiedostot välimuistiin
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivoituminen
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
