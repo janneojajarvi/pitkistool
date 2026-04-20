870c28c4e9284fa4521a579e33f610df4a807892
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-20 16:18:18 +0200

    Add files via upload
    extrasetti lisätty

---
+++ extrasetti5.js
@@ -0,0 +1,74 @@
+var extrasetti5 = [
+  {
+    name: "Grannas bastu",
+    abc: "X:1\nT: Grannas bastu\nR: Schottis\nO: Trad. Finland\nL:1/16\nM:2/4\nI:linebreak $\nK:Ador\nV:1\n A2A2 ABcd | e2e2 c2ef | g2fg agfg | e4 c2>B2 |\n$ A2A2 ABcd | efed cdef | g2fg agfg | e4 c4 ||$ %8\n cdeg f2ed | e2c2 A2AB | BGBd c2B2 | A4 A2e2 |$ \neceg f2ed | e2c2 A2AB | BGBd c2B2 | A4 A4 |] %16"
+  },
+  {
+    name: "Hoijakka",
+    abc: "X:1\nT: Hoijakka\nR: Hoijakka\nO: Trad. Finland\nL:1/8\nM:3/4\nI:linebreak $\nK:C\nV:1\n GG/c/ ee/d/ cc | BB/d/ ff/e/ dd |$ GG/c/ ee/d/ cc | ba g2 g2 |\n|:$ ff/g/ aa/g/ ff | ee/f/ gg/f/ ee |$ dd/e/ ff BB | dd c2 c2 :| %8"
+  },
+  {
+    name: "Niittykosken polkka",
+    abc: "X:1\nT: Niittykosken polkka\nR: Polka\nO: Trad. Finland\nL:1/8\nM:2/4\nI:linebreak $\nK:G\nV:1\n GB/B/ BB | BA/B/ cB | Ac/c/ cc | cB/c/ dc |\n$ Bd/d/ gg | gf fe | dd d/c/A/F/ |1 GG G2 :|2$ GG Gg ||\n|: bg gf/g/ | a/g/f fe/f/ | g/f/e e/f/g/a/ |$ bB B2 |\n bg gf/g/ | a/g/f fe/f/ |\n$ g/f/e e/f/g/f/ |1 e2 e>g :|2 e2 d2 |] %18"
+  },
+  {
+    name: "Oravaisten menuetti",
+    abc: "X:1\nT:Oravaisten menuetti\nR: Menuet\nO: Trad. Finland\nL:1/8\nM:3/4\nI:linebreak $\nK:A\nV:1\n c>d cB Ac | e>f ed ce |$ a>b ag fe/f/ | gf e2 e2 :|\n|:$ a>a ag f2 | e>f eA c2 |$ e>e eA BB/d/ | cB A2 A2 :| %8"
+  },
+  {
+    name: "Peli-Jussin syömämarssi",
+    abc: "X:1\nT: Peli-Jussin syömämarssi\nO: Trad. Finland\nR: March\nL:1/16\nM:4/4\nI:linebreak $\nK:A\nV:1\n eac'a b2b2 bac'a b2b2 | f2af gab2 bagf e2e2 |\n$ eac'a b2b2 bac'a b2b2 | f2af gaba c'2c'b c'2 z2 :|\n|:$ c'bc'e' d'c'd'b c'bc'e' d'c'd'b | f2af gab2 bagf e2e2 |\n$ c'bc'e' d'c'd'b c'bc'e' d'c'd'b | f2af gaba c'2c'b c'2 z2 :| %8"
+  },
+  {
+    name: "Pinnin polska",
+    abc: "X:1\nT: Pinnin polska\nO: Trad. Finland\nR: Polska\nL:1/16\nM:3/4\nI:linebreak $\nK:D \nV:1\n A2d2 d2eg f2ed | c2A2 Acec Acec |\n$ B2>B2 d2c2 B2A2 | BABc A2B2 c2A2 |$ A2d2 d2eg f2ed | %5\n c2A2 Acec Acec |$ B2>B2 d2c2 B2A2 | BABc A4 A4 :|\n|:$ A2a2 afdf afdf | g2g2 gece gece |$ %10\n g2ba gagf efed | c2d2 e2ge dcBA |\n$ A2a2 afdf afdf | g2g2 gece gece |\n$ g2ba gagf efed | c2Ac e4 d4 :| %16"
+  },
+  {
+    name: "Pinnin valssi",
+    abc: "X:1\nT: Pinnin valssi\nO: Trad. Finland\nR: Waltz\nL:1/8\nM:3/4\nI:linebreak $\nK:C\nV:1\n GB dB dB | GB dB dB | Gc ec ec | Gc ec ec |\n$ GB dB dB | GB dB dB | A2 AG AB | G4 G2 :|\n|:$ G2 ff ff | f2 e2 d2 | c3 d ef | g4 e2 |\n$ f3 e dc | B4 G2 | A2 AG AB | G4 G2 :| %16"
+  },
+  {
+    name: "Polonessa Spoof",
+    abc: "X:1\nT: Polonessa Spoof\nO: Trad. Finland\nR: Polonaise\nL:1/8\nM:3/4\nI:linebreak $\nK:A\nV:1\n GA |: Bc AG GA | Bc B3 B/c/ | de cB AG |$ GA A2 GA |\n Bc AG GA | Bc B3 B/c/ |$ de cB AG |1 GA A2 GA :|\n|2 GA A2 B/c/d |:$ cd e2 e2 | fe ed dB |\n cd e2 e2 |$ fe ed dB |\n (3cBA G2 A2 | Bc B3 B/c/ |$ de (3c/d/c/B AG |\n|1 GA A2 B/c/d :|2 GA A2 z2 |] %19"
+  },
+  {
+    name: "Raunon valssi",
+    abc: "X:1\nT: Raunon valssi\nT:(Långdans efter Nämndemans Ola)\nO: Trad. Sweden, Småland\nR: Waltz\nL:1/8\nM:3/4\nI:linebreak $\nK:Ador\nV:1\n A2 cB BA | B2 dc Bc |$ A2 AB cd | e2 ef e2 |\n$ A2 cB BA | B2 dc Bc |$ A2 AB cd | e4 e2 ||$ %8\n e2 gf fe | e2 c2 c2 |$ cB dc Bc | A2 AB cd |\n$ e2 gf fe | e2 c2 c2 |$ cB dc Bc | A2 A4 |] %16"
+  },
+  {
+    name: "Rintalan Heikin polska",
+    abc: "X:1\nT: Rintalan Heikin polska\nO: Trad. Finland\nR: Polska\nL:1/16\nM:3/4\nI:linebreak $\nK:G\nV:1\n gabg fgaf gdef | gabg fgaf g2d2 |\n$ g2fe d2c2 B2d2 | c2AF G4 G4 :|\n|:$ c4 c2Bc e2ce | g2fe d2c2 B2d2 |\n$ g2fe edcA AdBG | BcAF G4 G4 :| %8"
+  },
+  {
+    name: "Sandy's Boys",
+    abc: "X:1\nT: Sandy's Boys\nO: Trad. USA\nR: Polska\nL:1/8\nM:2/4\nI:linebreak $\nK:Amix\nV:1\n AG EG | E/D/C A,2 | AG EG | A4 |\n$ AG EG | E/D/C A,2 | A,E DC | A,4 :|\n|:$ E2 E2 | DC A,2 | A,E DC | A,E DC |\n$ E2 E2 | DC A,2 | A,E DC | A,4 :| %16"
+  },
+  {
+    name: "Schottis från Övermark",
+    abc: "X:1\nT: Schottis från Övermark\nO: Trad. Finland\nR: Schottis\nL:1/8\nM:2/4\nI:linebreak $\nK:Ador\nV:1\n A2 AB | c2 cA | d2 cB | A2 AB |\n$ c2 cA | d2 dB | e4 | e4 :|\n|:$ a2 af | d2 df | e2 ec | A2 AB |\n$ c2 c2 | cB GB | A4 | A4 :|"
+  },
+  {
+    name: "Seijarin polska",
+    abc: "X:1\nT: Seijarin polska\nO: Trad. Finland\nR: Polska\nL:1/16\nQ:1/4=100\nM:3/4\nI:linebreak $\nK:G\nV:1 treble \nV:1\n g2ba g2>e2 e2d2 | d2ef gfge e2d2 |$ d2gf g2B2 A2c2 |\n|1 BcAF G4 G4 :||2 BcAF G4 G2A2 |:$ BABd edBd c2Bc |\n$ A2AB c2Ac B2G2 | B2d2 edBd c2Bc |$ A2AB c2Ac B2G2 |\n G2gf g2B2 A2c2 |1 BcAF G4 G2A2 :|2 BcAF G4 G4 |] %12"
+  },
+  {
+    name: "Syrjälän Kaappoon polska",
+    abc: "X:1\nT: Syrjälän Kaappoon polska\nO: Trad. Finland\nR: Polska\nL:1/16\nM:3/4\nI:linebreak $\nK:D\nV:1\n ABAG F2G2 A2Bc | d2ce d2c2 B2A2 |$ B2BA G2F2 E2D2 | CDEC A,4 D4 :|\n|:$ E2ED CDEF G2E2 | D2DC DEFG A2F2 |$ E2ED CDEF G2E2 |\n D2DC DEFG A2F2 |$ B2BA G2F2 E2D2 | CDEC A,4 D4 :| %10"
+  },
+  {
+    name: "Tutskovin polska",
+    abc: "X:1\nT: Tutskovin polska\nO: Trad. Finland\nR: Polska\nL:1/4\nM:2/4\nI:linebreak $\nK:C\nV:1\n E C | E C | F E | D C |$ D D | C D | E C | C2 :|\n|:$ G2 | E D/E/ | F E | D C |$ D D | C D | E C | C2 :| %16"
+  },
+  {
+    name: "Vakavanha Väinämöinen",
+    abc: "X:1\nT: Vakavanha Väinämöinen\nO: Trad. Finland\nR: Song\nL:1/8\nM:5/4\nI:linebreak $\nK:Am\nV:1\n AABBce B2 B2 | ddccBc A2 A2 ::$ eeddec B2 B2 | ddccBc A2 A2 :| %4"
+  },
+  {
+    name: "Vals från Åsen",
+    abc: "X:1\nT: Vals från Åsen\nO: Trad. Sweden, Åsen\nR: Vals\nL:1/8\nM:3/4\nI:linebreak $\nK:Ador\nV:1\n AA |: A2 cA cd | e4 e2 | e2 f2 d2 |$ c4 cd |\n c2 BG BG |1 A4 AA :|2 A3 A cd |:$ ef e2 d2 | %9\n f2 ed cd | ef e2 d2 |$ f2 ed cd | ef ed fd |\n e3 A cd |$ e2 f2 d2 | c4 cd |\n c2 BG BG |1 A3 A cd :|2 A4 z2 |] %19"
+  },
+  {
+    name: "Vanha vinteriska",
+    abc: "X:1\nT: Vanha vinteriska\nO: Trad. Finland\nR: Schottis\nL:1/8\nM:2/4\nI:linebreak $\nK:C\nV:1\n CC/D/ EE/F/ | GG GA/B/ | cC/D/ EE | ED/C/ D2 |\n$ G,G,/B,/ DD/E/ | FF/G/ AG/F/ | EC/E/ DB,/D/ | DC/B,/ C2 :|\n|:$ c>B AG | .FA FA | d>c BA | GF EG |\n$ G,G,/B,/ DD/E/ | FF/G/ AG/F/ | EC/E/ DB,/D/ | DC/B,/ C2 :| %16"
+  }
+    ];
