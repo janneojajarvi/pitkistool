e768fcdc37609bf9951b0edabf535b5ed15d2d56
janneojajarvi <janne.ojajarvi@gmail.com>
2026-04-30 18:11:46 +0200

    Add files via upload

---
+++ folkwikiExtra.js
@@ -0,0 +1,14 @@
+ var folkwikiExtra = [
+  {
+    name: "Gammal schottis efter Bengt Håkansson",
+    abc: "X:1\nT:Gammal schottis efter Bengt Håkansson\nT:(SmM V-48)\nR:Schottis\nO:Jät, Småland\nS:Bengt Håkansson\nS:August Strömberg\nN:Ca 1890. Upptecknad av August Strömberg.\nB:Småländsk Musiktradition\nM:2/4\nL:1/8\nK:Dm\n(a>f) dd|c(e/c/) AA|(a>f) dd|c(e/c/) AA|(d/^c/)d/e/ (f/e/)f/g/|(a/g/)f/e/ fd|\ne(f/e/) AB/^c/|de d2:| |:(d>c) cA|(c>e) g2|(a/b/)a/g/ (f/e/)d/^c/|(d/e/)f/g/ e2|\n(d>c) cA|(c>e) g2|(a/b/)a/g/ (f/e/)d/^c/|de d2:|"
+  },
+  {
+    name: "Polonäs nr 72 från Sexdrega",
+    abc: "X:1\nT:Polonäs nr 72 från Sexdrega\nC:Trad. soitt. Johannes Bryngelsson, Sexdrega (Västra Götaland)\n%%scale 0.83\n%%pageheight 29.69cm\n%%pagewidth 21.01cm\n%%leftmargin 1.50cm\n%%rightmargin 1.50cm\n%%topmargin 1.50cm\n%%botmargin 1.50cm\nL:1/16\nM:3/4\nI:linebreak $\nK:A\nV:1 treble nm=\"Violin\" snm=\"Vln.\"\nV:1\n a2ba e2fe d2ed | c2BA e6 e2 | fefg f2e2 d2c2 | B2AG A8 |$ a2ba e2fe d2ed | c2BA e6 e2 | %6\n fefg f2e2 d2c2 | B2AG A6 E2 |:$ E2EG A4 B4 | c2B2 A2G2 A2E2 | C2A,2 C2E2 A2d2 | cdcB A4 g4 | %12\n fgfe d2c2 d2B2 | efed cdcB A4 |$ f4 fgfe dcdB | efed cdcB A2a2 | g2a2 b4 a2g2 | a2ec A8 :| %18"
+  },
+  {
+    name: "Schottis efter Bengt Håkansson",
+    abc: "X:1\nT:Schottis efter Bengt Håkansson\nC:Trad. Ruotsi\n%%scale 0.83\n%%pageheight 29.69cm\n%%pagewidth 21.01cm\n%%leftmargin 1.50cm\n%%rightmargin 1.50cm\n%%topmargin 1.50cm\n%%botmargin 1.50cm\nL:1/16\nM:2/4\nI:linebreak $\nK:D\nV:1 treble nm=\"Violoncello\" snm=\"Vc.\"\nV:1\n f2>d2 B2B2 | B>Ac>A F2F2 | f2>d2 B2B2 | B>Ac>A F2B2- | B>AB>c d>cd>e |$ f>gf>e f2>c2- | %6\n c2e>c A2F>A |1 B-BB2 B>cd>e :|2 B2B2 B4 |:$ B2A2 A2F2 | A2c2 e2>f2- | f>gf>e d>cB>A | %12\n B>cd>e c2c2 | B2A2 A2F2 | A2c2 e2>f2- |$ f>gf>e d>cB>A | c2B2 B4 :| %17"
+  }
+    ];
