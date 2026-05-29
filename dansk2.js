0b0123ab96a65c8ff6c31714e0983966ab008efa
janneojajarvi <janne.ojajarvi@gmail.com>
2026-05-29 22:28:25 +0300

    Update dansk2.js

--- dansk2.js
+++ dansk2.js
@@ -1,22 +1,18 @@
 var dansk2 = [
   {
-    name: "Bette Met
-Mors",
-    abc: "M:2/4\nL:1/8\nK:D\nd ^f A A | d ^f A A | d d e e | ^f3 e2 | d ^f A A | d ^f A A | d d e e | ^f2 z2 | d d d d | d ^f ^f e | ^c ^c ^c ^c | ^c e e ^c | d B B B | ^c A A A | B B ^c ^c | d2 z2 |"
+    name: "Bette Met Mors",
+    abc: "X:1\nT:Bette Met Mors\nM:2/4\nL:1/8\nK:D\nd ^f A A | d ^f A A | d d e e | ^f3 e2 | d ^f A A | d ^f A A | d d e e | ^f2 z2 | d d d d | d ^f ^f e | ^c ^c ^c ^c | ^c e e ^c | d B B B | ^c A A A | B B ^c ^c | d2 z2"
   },
   {
-    name: "Den evig glade Kobbersmed
-   Galop-/hoptrin
-              Kæppedansen",
-    abc: "M:4/4\nL:1/8\nK:C\nE2 | G2 A2 G2 E2 | G2 A2 G2 E2 | G2 F2 F2 E2 | F4 z2 c2 | B4 B2 d2 | A4 A2 B2 | A2 G2 G2 E2 | G4 z2 E2 | G2 A2 G2 E2 | G2 A2 G2 E2 | G2 ^F2 ^F2 E2 | ^F4 z2 D2 | G2 G2 G2 A2 | B2 B2 B2 B2 | A2 c2 B2 A2 | G2 z2 E3 F | G4 e4 | e4 d3 c2 | c2 B2 B2 A2 | B2 z2 D3 E | F4 c4 | c2 z2 B3 A | A2 G2 G2 E2 | G2 z2 E3 F | G4 e4 | e2 z2 d3 c2 | c2 B2 B2 A2 | B2 z2 D3 E | F2 z2 B3 A | G2 z2 e3 d2 | c2 G2 B2 G2 | c2 z4 |"
+    name: "000 Den evig glade Kobbersmed den to stemmige",
+    abc: "X:1\nT:000 Den evig glade Kobbersmed den to stemmige\nM:4/4\nL:1/8\nK:C\nE2 | G2 A2 G2 E2 | G2 A2 G2 E2 | G2 F2 F2 E2 | F4 z2 c2 | B4 B2 d2 | A4 A2 B2 | A2 G2 G2 E2 | G4 z2 E2 | G2 A2 G2 E2 | G2 A2 G2 E2 | G2 ^F2 ^F2 E2 | ^F4 z2 D2 | G2 G2 G2 A2 | B2 B2 B2 B2 | A2 c2 B2 A2 | G2 z2 E3 F | G4 e4 | e4 d3 c2 | c2 B2 B2 A2 | B2 z2 D3 E | F4 c4 | c2 z2 B3 A | A2 G2 G2 E2 | G2 z2 E3 F | G4 e4 | e2 z2 d3 c2 | c2 B2 B2 A2 | B2 z2 D3 E | F2 z2 B3 A | G2 z2 e3 d2 | c2 G2 B2 G2 | c2 z4"
   },
   {
-    name: "Engelsk Dans
-Op og ned",
-    abc: "M:2/4\nL:1/8\nK:G\ng2 B c | d e d2 | d2 B G | A B c2 | c2 A G | ^F G A ^F | D ^F A ^F | G2 z2 | z4 | z4 | z4 | a4 | ^f3 d2 | A d ^f d | A d ^f a | g4 | e3 ^c2 | A ^c e ^c | d2 z2 |"
+    name: "Engelsk Dans Op og ned",
+    abc: "X:1\nT:Engelsk Dans Op og ned\nM:2/4\nL:1/8\nK:G\ng2 B c | d e d2 | d2 B G | A B c2 | c2 A G | ^F G A ^F | D ^F A ^F | G2 z2 | z4 | z4 | z4 | a4 | ^f3 d2 | A d ^f d | A d ^f a | g4 | e3 ^c2 | A ^c e ^c | d2 z2"
   },
   {
-    name: "Arr.: H P S",
-    abc: "M:2/4\nL:1/8\nK:E\ne ^d e ^d | e2 B2 | ^G2 B2 | ^G2 E2 | ^F ^E ^F ^E | ^F2 ^D2 | B,2 ^C ^D | E2 z2 | B2 ^g2 | B2 B2 | B2 a2 | a ^g ^f2 z | e2 z2 |"
+    name: "Nimetön",
+    abc: "X:1\nT:Nimetön\nM:2/4\nL:1/8\nK:E\ne ^d e ^d | e2 B2 | ^G2 B2 | ^G2 E2 | ^F ^E ^F ^E | ^F2 ^D2 | B,2 ^C ^D | E2 z2 | B2 ^g2 |  |  | B2 B2 | B2 a2 | a ^g ^f2 z |  | e2 z2"
   }
 ];
