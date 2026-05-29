711a22abcd948930f8a8333b41bae90f953ff0ad
janneojajarvi <janne.ojajarvi@gmail.com>
2026-05-29 22:21:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ var urls = [
 	{ type: "Sekalaiset", url: "swedish2.js" },
 	{ type: "Sekalaiset", url: "norway1.js" },
 	{ type: "Sekalaiset", url: "norway1.js" },
+	{ type: "Sekalaiset", url: "dansk2.js" },
 	{ type: "Sekalaiset", url: "nordbeck.js" },
 	{ type: "Sekalaiset", url: "richardrobinsonbook.js" },
 	{ type: "Folkwikisetit", url: "folkwikiExtra.js" },
