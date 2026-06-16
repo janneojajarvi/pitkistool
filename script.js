0b0b6765b606cb23fedbc85b84031beefb18af4c
janneojajarvi <janne.ojajarvi@gmail.com>
2026-06-16 23:46:32 +0300

    Add new script entry for 'own.js'

--- script.js
+++ script.js
@@ -69,6 +69,7 @@ var urls = [
 	{ type: "Sekalaiset", url: "nordbeck.js" },
 	{ type: "Sekalaiset", url: "richardrobinsonbook.js" },
 	{ type: "Sekalaiset", url: "abctransposer1.js" },
+	{ type: "Sekalaiset", url: "own.js" },
 	{ type: "Folkwikisetit", url: "folkwikiExtra.js" },
         { type: "Folkwikisetit", url: "folkwikiSet1.js" },
         { type: "Folkwikisetit", url: "folkwikiSet2.js" },
